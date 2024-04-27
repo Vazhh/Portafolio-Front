654583c9d7208b166d9357a53808d6f876ea1fe7
Gerardo  Aguirre <rg.aguirre93@gmail.com>
2024-04-27 11:21:10 -0500

    limpieza de espacion

--- Portafolio-Front/src/app/theme/ThemeSlice.tsx
+++ Portafolio-Front/src/app/theme/ThemeSlice.tsx
@@ -1,13 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
-// import type { PayloadAction } from '@reduxjs/toolkit'
-// import type { RootState } from '../Store'
 
 // Define a type for the slice state
 interface ThemeState {
   darkMode: boolean
 }
-
-// Define the initial state using that type
 
 const storedDarkMode = localStorage.getItem('darkMode') ;
 let initialDarkMode = false;
@@ -30,7 +26,6 @@ export const themeSlice = createSlice({
   initialState,
   reducers: {
 
-    // Use the PayloadAction type to declare the contents of `action.payload`
     changeTheme: (state) => {
       state.darkMode = !state.darkMode;
       localStorage.setItem('darkMode',
@@ -40,9 +35,4 @@ export const themeSlice = createSlice({
 })
 
 export const { changeTheme } = themeSlice.actions
-
-
-// Other code such as selectors can use the imported `RootState` type
-// export const selectCount = (state: RootState) => state.counter.value
-
 export default themeSlice.reducer
