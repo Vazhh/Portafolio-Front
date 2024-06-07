821df4011a299e7c723de6c07b5e155d76c74cd2
Gerardo  Aguirre <rg.aguirre93@gmail.com>
2024-06-06 22:22:36 -0500

    se termina el componente de skills, pendiente acomodar tamaños para las demas vistas, se adiciona rutas de linkedin y github y se corrige error en margin que descuadraban la vista

--- src/pages/Principal.tsx
+++ src/pages/Principal.tsx
@@ -5,7 +5,7 @@ import Skills from "../components/Skills"
 
 export default function Principal() {
   return (
-    <div className="absolute inset-0 flex items-center justify-center">
+    <div className="absolute inset-0 flex items-center justify-center w-full p-0 m-0">
       <Main/>
       {/* <Introduction/> */}
       <Skills/>
